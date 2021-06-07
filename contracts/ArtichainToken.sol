5ba5bc5b278c879d83e77108f178f18bfe75b507
risingstar2018 <78557832+ArtiChain@users.noreply.github.com>
2021-06-08 01:46:23 +0800

    Update ArtichainToken.sol

--- contracts/ArtichainToken.sol
+++ contracts/ArtichainToken.sol
@@ -6,7 +6,7 @@ import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
 // ArtichainToken with Governance.
 contract ArtichainToken is BEP20('Artichain Token', 'AIT') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterArt).
-    function mint(address _to, uint256 _amount) external onlyOwner {
+    function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
         _moveDelegates(address(0), _delegates[_to], _amount);
     }
